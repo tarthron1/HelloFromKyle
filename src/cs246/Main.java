d16483fadb9593352da3a1ab64ba73be8573251c
Hannah Rogers <rog19008@byui.edu>
2020-05-14 15:08:15 -0600

    Hello from Hannah

--- src/cs246/Main.java
+++ src/cs246/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello my name is Kyle.");
+        System.out.println("Hello Kyle, my name is Hannah");
     }
 }
